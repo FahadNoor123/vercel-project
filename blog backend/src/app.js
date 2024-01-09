76c364e7a558ba248f2b18317156c15ae59ac63d
Fahad noor <fahadnoor6677@gmail.com>
2024-01-09 23:00:57 +0500

    corrent index and app file

--- blog backend/src/app.js
+++ blog backend/src/app.js
@@ -1,27 +1,19 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from './routes/user.routes.js';
+import blogRouter from "./routes/blog.routes.js";
 
-const app = express()
+const app = express();
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+app.use(cors());
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
-
-//routes import
-import userRouter from './routes/user.routes.js'
-import blogRouter from "./routes/blog.routes.js"
-
-
-//routes declaration
-app.use("/api/v1/users", userRouter)
-// Use the blogRouter for blog-related routes under the /api/v1/writeblog namespace
+app.use("/api/v1/users", userRouter);
 app.use("/api/v1/blog", blogRouter);
 
-// http://localhost:8000/api/v1/users/register
-
-export { app }
+export { app };
