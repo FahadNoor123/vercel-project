5e3aef6a268310b9d1af5e18d4c1bd9289b4ee44
Fahad noor <fahadnoor6677@gmail.com>
2024-01-10 20:53:07 +0500

    domin is dded

--- blog backend/src/controllers/user.controller.js
+++ blog backend/src/controllers/user.controller.js
@@ -131,8 +131,8 @@ const loginUser = asyncHandler(async (req, res) => {
         path: "/",
       };
   
-      res.cookie('accessToken', accessToken, { domain: 'https://vercel-project-kappa.vercel.app/', secure: true, sameSite: 'None' });
-      res.cookie('refreshToken', refreshToken, { domain: 'vercel.app', secure: true, sameSite: 'None' });
+      res.cookie('accessToken', accessToken, { domain: 'https://vercel-project-kappa.vercel.app', secure: true, sameSite: 'None' });
+      res.cookie('refreshToken', refreshToken, { domain: 'https://vercel-project-kappa.vercel.app', secure: true, sameSite: 'None' });
       
   
       // Log a message indicating that cookies are being set
