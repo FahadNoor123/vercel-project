aef9d4ddf53173aa90397899a62ef6479d83831d
Fahad noor <fahadnoor6677@gmail.com>
2024-01-11 18:08:56 +0500

    cookies domain updated

--- blog backend/src/controllers/user.controller.js
+++ blog backend/src/controllers/user.controller.js
@@ -132,7 +132,7 @@ const loginUser = asyncHandler(async (req, res) => {
       };
   
       res.cookie('accessToken', accessToken, { domain: 'vercel-project-kappa.vercel.app', secure: true, sameSite: 'None' });
-      res.cookie('refreshToken', refreshToken, { domain: 'https://vercel-project-kappa.vercel.app', secure: true, sameSite: 'None' });
+      res.cookie('refreshToken', refreshToken, { domain: 'vercel-project-kappa.vercel.app', secure: true, sameSite: 'None' });
       
   
       // Log a message indicating that cookies are being set
