6c5b9f12a1f8f19eadff968601c4b1d0be3ee90e
Fahad noor <fahadnoor6677@gmail.com>
2024-01-11 20:04:33 +0500

    vite config file updated

--- blog front end/vite.config.js
+++ blog front end/vite.config.js
@@ -1,23 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://vercel-project-backend.vercel.app/',
+        target: 'https://vercel-project-backend.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    },
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
     },
   },
 });
