7b9643d98297e582ac63d76c8d4b09580a3c7c67
Fahad noor <fahadnoor6677@gmail.com>
2024-01-11 21:59:40 +0500

    hhhhhjh

--- blog front end/src/components/yourBlog/YourBlog.jsx
+++ blog front end/src/components/yourBlog/YourBlog.jsx
@@ -9,7 +9,7 @@ import { Link,  NavLink } from 'react-router-dom';
         try {
           const response = await fetch('https://vercel-project-backend.vercel.app/api/v1/blog/yourblog', {
             method: 'GET',
-            credentials: 'include',
+          
           });
   
           if (response.ok) {
